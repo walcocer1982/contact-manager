ceabf2317f3655fc94927c03ee1f03a2318c21fb
Walther Enriquer Alcocer Gutierrez <walcocer.1982@gmail.com>
2025-03-05 22:12:49 -0500

    feat: Implementación de formulario con validaciones y mejoras en el diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import { useState } from 'react';
 import ContactList from './components/ContactList';
 import ContactPinned from './components/ContactPinned';
+import ContactForm from './components/ContactForm';
 import Header from './components/Header';
 import contacts from './data/contacts.json';
 import './ContactList.css';
 
 const App = () => {
   const [highlightedContact, setHighlightedContact] = useState(null);
+  const [allContacts, setAllContacts] = useState(contacts);
 
   const handleClearContact = () => {
     setHighlightedContact(null);
   };
 
+  const handleAddContact = (newContact) => {
+    setAllContacts(prevContacts => [...prevContacts, newContact]);
+  };
+
   return (
-    <div>
+    <div className="app-container">
       <Header />
-      <ContactList contacts={contacts} onSelectContact={setHighlightedContact} />
-      {highlightedContact && (
-        <ContactPinned contact={highlightedContact} onClearContact={handleClearContact} />
-      )}
+      <div className="main-content">
+        <div className="column">
+          <ContactForm onAddContact={handleAddContact} />
+        </div>
+        <div className="column">
+          <ContactList contacts={allContacts} onSelectContact={setHighlightedContact} />
+        </div>
+        <div className="column">
+          {highlightedContact && (
+            <ContactPinned contact={highlightedContact} onClearContact={handleClearContact} />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
